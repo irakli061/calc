e6e96e1359c3b7df00b39c56c15800245390e1d4
irakli061 <irakliimnaishvili13@gmail.com>
2023-02-15 00:14:09 +0400

    general little changes

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
+const x = 1;
+x = 2;
+
 function plus() {
   let number1 = document.getElementById("Number1");
   let number2 = document.getElementById("Number2");
   let answer = document.getElementById("answer");
-  answer.innerHTML = Number(number1.value) + Number(number2.value);
+  let result = Number(number1.value) + Number(number2.value);
+  answer.innerHTML = result;
 }
 
 function minus() {
   let number1 = document.getElementById("Number1");
   let number2 = document.getElementById("Number2");
   let answer = document.getElementById("answer");
-  answer.innerHTML = number1.value - number2.value;
+  let result = Number(number1.value) - Number(number2.value);
+  answer.innerHTML = result;
 }
 
 function multiply() {
   let number1 = document.getElementById("Number1");
   let number2 = document.getElementById("Number2");
   let answer = document.getElementById("answer");
-  answer.innerHTML = number1.value * number2.value;
+  let result = Number(number1.value) * Number(number2.value);
+  answer.innerHTML = result;
 }
 
 function devide() {
   let number1 = document.getElementById("Number1");
   let number2 = document.getElementById("Number2");
   let answer = document.getElementById("answer");
-  answer.innerHTML = number1.value / number2.value;
+  let result = Number(number1.value) / Number(number2.value);
+  answer.innerHTML = result.toFixed(3);
 }
 
 function DELL() {
